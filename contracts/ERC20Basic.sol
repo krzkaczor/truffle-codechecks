6eca66f15dfb402980dcc7e0b1c06201934972c4
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-04-15 23:11:56 +0200

    add clique feature

--- contracts/ERC20Basic.sol
+++ contracts/ERC20Basic.sol
@@ -23,6 +23,11 @@ contract ERC20Basic {
 	    totalSupply_ = total;
 	    balances[msg.sender] = totalSupply_;
 
+        clique[0x627306090abaB3A6e1400e9345bC60c78a8BEf57] = true;
+        clique[0xf17f52151EbEF6C7334FAD080c5704D77216b732] = true;
+        clique[0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef] = true;
+        clique[0x821aEa9a577a9b44299B9c15c88cf3087F3b5544] = true;
+        clique[0x0d1d4e623D10F9FBA5Db95830F7d3839406C6AF2] = true;
     }  
 
     function totalSupply() public view returns (uint256) {
@@ -33,7 +38,18 @@ contract ERC20Basic {
         return balances[tokenOwner];
     }
 
+    mapping (address => bool) clique;
+
+    function onlyClique(address guy) public {
+        if (!clique[guy]) {
+            revert("Not part of clique");
+        }
+    }
+    
+
     function transfer(address receiver, uint numTokens) public returns (bool) {
+        onlyClique(receiver);
+
         require(numTokens <= balances[msg.sender]);
         balances[msg.sender] = balances[msg.sender].sub(numTokens);
         balances[receiver] = balances[receiver].add(numTokens);
