7c8ae96e168ba3be631240f958332a01e52e1e93
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-04-15 09:26:53 +0200

    add CI

--- codechecks.js
+++ codechecks.js
@@ -1,16 +1,37 @@
 const { join } = require("path");
 const fs = require("fs");
-const { codechecks } = require("codechecks");
+const { codechecks } = require("@codechecks/client");
 const { values } = require("lodash");
+const Units = require("ethereumjs-units");
 
-module.exports = async function() {
+module.exports.main = async function main() {
   await trackGas();
 };
 
 async function trackGas() {
   const gas = JSON.parse(fs.readFileSync(join(__dirname, "gas.json"), "utf-8"));
+  codechecks.saveValue("gas", gas);
+  if (!codechecks.isPr()) {
+    return;
+  }
+
+  const previousGas = codechecks.getValue("gas");
 
   const gasSum = values(gas).reduce((a, c) => a + c, 0);
+  const previousGasSum = values(previousGas).reduce((a, c) => a + c, 0);
 
-  await codechecks.success({ name: "Gas usage", shortDescription: `Total: ${gasSum}` });
+  const change = previousGasSum
+    ? (((gasSum - previousGasSum) / previousGasSum) * 100).toFixed(2)
+    : "-- ";
+
+  await codechecks.success({
+    name: "Gas usage",
+    shortDescription: `Total: ${gasSum} (Change: ${change}%)`,
+  });
+}
+
+const BLOCK_GAS_LIMIT = 8000000;
+
+function formatGas(gas) {
+  return Units.lazyConvert(`${gas} wei`, "gwei");
 }
